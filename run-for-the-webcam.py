61019eb04bb04ded737c405ec97e50b2d5c2adcf
henrique.alves <henri.falves@hotmail.com>
2019-10-18 01:56:09 -0300

    [Rick] - add try to catch exception of KeyBoardInterrupt type

--- run-for-the-webcam.py
+++ run-for-the-webcam.py
@@ -4,6 +4,7 @@ import webbrowser
 import requests
 import json
 import datetime
+import sys
 # Windows dependencies
 # - Python 2.7.6: http://www.python.org/download/
 # - OpenCV: http://opencv.org/
@@ -15,25 +16,53 @@ import datetime
 # - pip install numpy
 # - brew tap homebrew/science
 # - brew install opencv
+
+def is_http_status_ok(status_code):
+    return status_code == 200
+
 def send_json(body):
     apiUrl = "https://run-ford-future-fiap.herokuapp.com/api/v1/images"
     r = requests.post(apiUrl, json=body)
-    print(r.status_code)
+    if is_http_status_ok(r.status_code):
+        print('image sended')
+    else:
+        print('error to send image [{}]'.format(r.status_code))
 
-cap = cv2.VideoCapture(0)
-formatBase64 = 'data:image/png;base64,'
-while(True):
-    ret, frame = cap.read()
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-    cv2.imshow('frame', rgb)
-    
+def get_requests():
+    apiUrl = "https://run-ford-future-fiap.herokuapp.com/api/v1/requests"
+    r = requests.get(apiUrl)
+    if is_http_status_ok(r.status_code):
+        return r.json()['data']
+    else:
+        return []
+
+def delete_request(id):
+    apiUrl = 'https://run-ford-future-fiap.herokuapp.com/api/v1/requests/{}'.format(id)
+    print(apiUrl)
+    r = requests.delete(apiUrl)
+    if is_http_status_ok(r.status_code):
+        print('request {} already deleted!'.format(id))
+    else:
+        print('error to delete request [{}]'.format(r.status_code))
+
+def take_photo():
+    formatBase64 = 'data:image/png;base64,'
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()    
     out = cv2.imwrite('capture.jpg', frame)
     
     with open("capture.jpg", "rb") as image:
         encoded_string = base64.b64encode(image.read()).decode()
-        webbrowser.open('{}{}'.format(formatBase64, encoded_string))
+        #webbrowser.open('{}{}'.format(formatBase64, encoded_string))
         send_json({'img_name': '{}'.format(datetime.datetime.now()), 'base64_img': '{}{}'.format(formatBase64, encoded_string) })
-    
-    break
-cap.release()
-cv2.destroyAllWindows()
+
+while(True):
+    try:
+        for request in get_requests():
+            if request['is_processed'] == False:
+                take_photo()
+                print(request['id'])
+                delete_request(request['id'])
+    except (KeyboardInterrupt, SystemExit):
+        print('cam verify stopped')
+        sys.exit()
