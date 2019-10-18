5aefe4f5169f88bf3b06cf7b3739e638022fe41c
henrique.alves <henri.falves@hotmail.com>
2019-10-18 05:49:17 -0300

    [Rick] Add facial rocoginization

--- run-for-the-webcam.py
+++ run-for-the-webcam.py
@@ -44,16 +44,28 @@ def delete_request(id):
         print('request {} already deleted!'.format(id))
     else:
         print('error to delete request [{}]'.format(r.status_code))
-
+    
 def take_photo():
+    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+    face_cascade.load('./haarcascade_frontalface_default.xml')
     formatBase64 = 'data:image/png;base64,'
     cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()    
-    out = cv2.imwrite('capture.jpg', frame)
+    ret, frame = cap.read()
     
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)    
+    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+    print('analysing image')
+    faces = face_cascade.detectMultiScale(gray,1.3, 5)
+    print(len(faces))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    out = cv2.imwrite('capture.jpg', cv2.medianBlur(frame, 3))
+
     with open("capture.jpg", "rb") as image:
         encoded_string = base64.b64encode(image.read()).decode()
-        #webbrowser.open('{}{}'.format(formatBase64, encoded_string))
         send_json({'img_name': '{}'.format(datetime.datetime.now()), 'base64_img': '{}{}'.format(formatBase64, encoded_string) })
 
 while(True):
